cb013907a168b614720570055615a529e54d4d67
Petar Randjelovic <petar.randjelovic@mitaway.com>
2022-06-02 17:14:22 +0200

    -

--- static/redirect.js
+++ static/redirect.js
@@ -1,5 +1,5 @@
 setTimeout(function(){
-    if(document.getElementById('emotion-detect').innerHTML.trim().replace(/&nbsp;/g, '') != 'stop'){
+    if(document.getElementById('emotion-detect').innerHTML.trim().replace(/&nbsp;/g, '') == 'none'){
         window.location.href = 'http://127.0.0.1:5000/scanned';
     }
     console.log(document.getElementById('emotion-detect').innerHTML.trim().replace(/&nbsp;/g, ''))
